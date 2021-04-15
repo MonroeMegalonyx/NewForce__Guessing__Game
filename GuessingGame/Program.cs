f0ecef57b004e3b6a0ee21fd0a1b8c7e7a4e7cd3
MonroeMegalonyx <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-04-15 15:49:42 -0400

    Completed Phase 2

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -2,16 +2,32 @@
 
 namespace GuessingGame
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-          // Writing a console program that invites the user to guess a number
-          // Display a message to the user asking them to guess the secret number.
-            Console.WriteLine("Welcome to Guessing Game");
-            Console.WriteLine("Guess the secret number");
-            string userGuess = Console.ReadLine();
-            Console.WriteLine(userGuess);
-        }
+      // Writing a console program that invites the user to guess a secret number
+      // Create a variable to contain the secret number.
+      int secretNumber = 42;
+
+      // Display a message to the user asking them to guess the secret number.
+      Console.WriteLine("Welcome to Guessing Game");
+      // Display a prompt for the user's guess.
+      Console.WriteLine("Guess the secret number");
+      // Take the user's guess as input and save it as a variable.
+      int userGuess = Convert.ToInt32(Console.ReadLine());
+      // Display the user's guess back to the screen. Hidden in Phase 2.
+      // Console.WriteLine(userGuess);
+
+      // Compare user's guess to the secret number and display an error or success message
+      if (userGuess == secretNumber ) 
+      {
+        Console.WriteLine("Wow you got it correct!");
+      }
+      else
+      {
+        Console.WriteLine("Wrong guess, can you do anything right?");
+      }
     }
+  }
 }
