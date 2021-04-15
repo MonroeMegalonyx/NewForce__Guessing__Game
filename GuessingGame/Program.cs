d3a6363b3248b2d2be996ce8eb8eda682e45e387
MonroeMegalonyx <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-04-15 15:43:19 -0400

    Completed Phase 1

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,7 +6,12 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          // Writing a console program that invites the user to guess a number
+          // Display a message to the user asking them to guess the secret number.
+            Console.WriteLine("Welcome to Guessing Game");
+            Console.WriteLine("Guess the secret number");
+            string userGuess = Console.ReadLine();
+            Console.WriteLine(userGuess);
         }
     }
 }
